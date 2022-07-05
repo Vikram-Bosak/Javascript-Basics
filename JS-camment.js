dce46da4a11a06dd9a8a4cb919d0f0f6013eaee2
Vikram Bosak <vikramkumarbosak69@gmail.com>
2022-07-05 23:06:41 +0530

    keep learning

---
+++ JS-camment.js
@@ -0,0 +1,4 @@
+// camments is s program to show all informations inside the code and its not show in brawser
+
+
+// .. dowble slace is use for cammenting in javascript
---
+++ JS-variables.js
@@ -0,0 +1,22 @@
+"use strick";
+
+// intro to veriables
+// variables can use store some information
+
+
+// we can use that infirmations later
+
+
+// Variables are containers for storing data (storing data values).
+
+
+ firstName = "Vikram";
+
+console.log(firstName);
+ 
+ firstname = "Mohit";
+ 
+console.log(firstname);
+
+
+// rules  of naming variables 
