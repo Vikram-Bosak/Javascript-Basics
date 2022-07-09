a4737c16ce23ca60a1a7af7fa016f09e060d0ccc
Vikram Bosak <vikramkumarbosak69@gmail.com>
2022-07-09 21:39:25 +0530

    keep learning

---
+++ JS-challenge-01.js
@@ -0,0 +1,20 @@
+// 1.---------------------------
+
+var _myName = "vikram";
+console.log(_myName);
+
+
+// 2.-----------------------------------
+
+var _1my_Name ="vikram";
+console.log(_1my_Name);
+
+// 3.--------------------------------------
+
+var 1myName = "vikram";
+console.log(1myName);
+
+// 4.------------------------------------------
+
+var $myName = "vikramBosak";
+console.log($myName);
--- JS-variables.js
+++ JS-variables.js
@@ -10,13 +10,23 @@
 // Variables are containers for storing data (storing data values).
 // the first charactor must be a letter or an underscore(_) or an doller ($) . you can not use a number as the first charactor
 
- var  firstName = "Vikram";
+//  var  firstName = "Vikram";
 
-console.log(firstName);
+// console.log(firstName);
  
-var  firstname = "Mohit";
+// var  firstname = "Mohit";
  
-console.log(firstname);
+// console.log(firstname);
 
 
 // rules  of naming variables 
+  
+// the rest of the variable name can include any letter any number or the underscore can't use any other characters,including spaces.
+
+// var myAge = 25;
+// console.log(myAge);
+
+// you can't use one of javascript reserved words as a variables name
+
+ var firstCar = "jeep and the thunder"
+console.log(firstCar);
