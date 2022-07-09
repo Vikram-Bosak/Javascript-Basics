71b70782408ef6c68fe8ad16802c5a409bc51dd9
Vikram Bosak <vikramkumarbosak69@gmail.com>
2022-07-09 23:08:43 +0530

    keep learning

---
+++ JS-Exercise-02.js
@@ -0,0 +1,58 @@
+// Data types practices
+
+// 1.  10 + "20"
+console.log(10 + "20");
+console.log(typeof(10 + "20"));
+
+
+
+// 2.  9 - "5"
+console.log ( 9 - "5");
+console.log(typeof(9 - "5")); 
+// ther is a bug 
+
+// 3.  "java"   +  "Script"
+console.log("java"   +  "Script");
+console.log(typeof("java"    +  " Script"));
+
+
+// 4.  " "  +  " "
+console.log(" "  +  " ");
+console.log(typeof(" "  +  " "));
+
+
+// 5.  " "  +  0
+console.log(" "  +  0);
+console.log(typeof(" "  +  0));
+
+
+
+// 6.  "vikram"  -  "bosak"
+console.log("vikram"  -  "bosak");
+console.log(typeof("vikram"  -  "bosak"));
+
+// its output is NAN``
+
+
+
+// 7.   true  + true
+console.log(true  + true);
+console.log(typeof(true  + true));
+
+// in javascript 1 =ture and 0 = false
+
+// 8. true  + false
+console.log(true  + false);
+console.log(typeof(true  + false));
+
+
+
+// 9.  false  + true
+console.log(false  + true);
+console.log(typeof(false  + true));
+
+
+
+// 10.  false - true 
+console.log(false - true);
+console.log(typeof(false - true));
---
+++ JS-data-types.js
@@ -0,0 +1,37 @@
+// six Date Types that are primitives
+
+// 1. indefind
+// 2. Boolean
+// 3. Number
+// 4. String
+// 5. Bigint
+// 6. Symbol
+
+
+// 4. ***** here are the string examples *****
+// var myName = " vikram Bosak";
+// console.log(myName);
+
+// console.log(typeof(myName));
+
+
+
+// ***** 3. Here are the examples of number data types in javascript
+// var myAge =24;
+// console.log(myAge);
+// console.log(typeof(myAge));
+
+
+// ***** 2. here are the examples of boolean data types in javascript
+
+// var iamvikram = true;
+// console.log(iamvikram);
+// console.log(typeof(iamvikram));
+
+// var iamvikrams = false;
+// console.log(iamvikrams);
+// console.log(typeof(iamvikrams));
+
+***** 
+
+
