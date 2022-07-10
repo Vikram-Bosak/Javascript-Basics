1a36d84e5a0fb07fa55804a8f245af9ddc56ac95
Vikram Bosak <107077620+Vikram-Bosak@users.noreply.github.com>
2022-07-10 09:07:29 +0000

    keep learning

--- JS-Expersions-and-operators.js
+++ JS-Expersions-and-operators.js
@@ -2,7 +2,34 @@
 //  the combination  of 0perand and opeattors is equal to experession
 
 // there are six types of experession and opeators
-1. Assignment operators
-2. Arithmetic operators
-3. comparison operators
-4. 
+
+// 1. Assignment operators
+
+// An Assignment operators assigns a value to its left operand based on the value of its right operand.
+// the simple Assignment operators is equal (=)
+
+
+
+
+// 2. Arithmetic operators
+// An Arithmetic operators takes numerical values ( either literals or variables ) as their operand return a single numerical values
+ 
+// console.log(3+3);
+// console.log(10-5);
+// console.log(20/5);
+// console.log(5*6);
+
+// console.log("remainder operator"   + 27%4);
+
+:
+
+
+// 3. comparison operators
+
+
+
+
+
+// 4. Logical operators
+// 5. String operators
+// 6. Conditional (ternary) operators
