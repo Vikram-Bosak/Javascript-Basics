ba8afa13023429cf841d1cb380d91893ae2f71e2
vikram-bosak <vikramkumarbosak69@gmail.com>
2022-07-11 23:55:04 +0530

    JS-Expersions-and-operators.js

--- JS-Expersions-and-operators.js
+++ JS-Expersions-and-operators.js
@@ -79,7 +79,21 @@ var b = -20;
 // Logical AND (&&)
 // the logical  AND (&&) operators (logical conjunction) for a set of operands is ture if and only if all of its operands are true.
 
-console. log(a > b && b > 0 && b < 0 );
+// console. log(a > b && b > 0 && b < 0 );
+
+
+// logical OR (||)
+// the logical OR (||) operator (logical disjunction) for a  set of operands is true if and only if one more of its operands is true
+
+// console. log(a > b || b > 0 || b < 0 );
+
+// Logical NOT (!)
+// the logical NOT (!) operator (logical complement , negation) take truth to falsity and vice Versa,
+
+console. log(!((a > b) && (b > 0) && (b < 0 )));
+
+
+
 
 // 5. String operators
 // 6. Conditional (ternary) operators
