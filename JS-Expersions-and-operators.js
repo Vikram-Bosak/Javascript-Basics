b88c930f69fd515a5cd474166f095d35500bf1f9
vikram-bosak <vikramkumarbosak69@gmail.com>
2022-07-12 00:19:58 +0530

    keep learning

--- JS-Expersions-and-operators.js
+++ JS-Expersions-and-operators.js
@@ -100,8 +100,13 @@ var b = -20;
 // 5. String concatenation operators
 // The concatenation operator (+) concatenates two string values together, returing another string that is the two operand string.
 
-console.log("Hello World");
-console.log("Hello" + "world");
+// console.log("Hello World");
+// console.log("Hello" + "world");
+
+// var myName = "vikram";
+
+// console.log(myName + "bosak");
+//  console.log(myName + " kumar Bosak");
 
 
 // 6. Conditional (ternary) operators
