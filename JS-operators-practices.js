ae6081b1481d6a59eabcd02869b3d0d5d28841a8
vikram-bosak <vikramkumarbosak69@gmail.com>
2022-07-12 12:42:45 +0530

    i have compate practices

---
+++ JS-operators-practices.js
@@ -0,0 +1,32 @@
+//  What is be the output of 3**3 ?
+console.log(3**3);  //(9*9)
+console.log(10**-1);   //(1/10)
+// Exponentiation operator (**) calculates the base to the exponent power that is , base exponent``
+
+
+
+
+// What will be the output , when we add a number and a string ?
+console.log(200 + "vikramBosak");
+
+
+
+// Write a progarm to swap two number?
+var a = 5;
+var b = 10;
+
+// output b =5 ; a =10; //⚾
+
+var c = b;  //c = 10 
+b = a;  // b = 5;
+a = c;
+
+console.log("the value of a is " + a);
+console.log("the value of b is " + b);
+
+
+
+// Write a program to swap two numbers without using third varibable?
+
+
+
