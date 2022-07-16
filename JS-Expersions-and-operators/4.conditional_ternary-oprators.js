7503bc6cdde15a96848d1fd035b810e43bb7c500
vikram-bosak <vikramkumarbosak69@gmail.com>
2022-07-17 02:03:24 +0530

    changesin a new files

---
+++ JS-Expersions-and-operators/4.conditional_ternary-oprators.js
@@ -0,0 +1,8 @@
+// => condiional (ternary) operator
+
+// -> codiitional (ternary) operator is the only javascript operator that takes three operands
+
+var age = 17;
+if (age >= ) {
+
+} else DD        
---
+++ JS-control-statement-&-loop/if-else/truthy-falsy.js
@@ -0,0 +1,15 @@
+// @ts-nocheck
+// => what is truthy and falsy values in javascript?
+
+// /  we have total 5 falsy values in javascript
+
+//  -0, " " ,  undefined ,null , NaN ,false *** is false anyway
+
+
+if (score = 0) {
+    console.log("OMG, We  loss the game ");
+
+} else {
+    console.log('yay, We  won the game');
+
+}
