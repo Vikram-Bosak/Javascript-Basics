6eaae4b075a9e3536b26117f2a9d91da0e9ec474
vikram-bosak <vikramkumarbosak69@gmail.com>
2022-07-16 22:33:45 +0530

    this is a ifelse program

--- JS-control-statement-&-loop/if-else/practices-statment-loop.js
+++ JS-control-statement-&-loop/if-else/practices-statment-loop.js
@@ -1,35 +1,25 @@
-// @ts-nocheck
-
-
 
 /// write a program that works out whether if a givern year is leap year or not ?
 
-/// A normal year has 365 days, leap years have 366 , with an extra day in february.
+/// A normal year has 365 days, leap years have 366 , with an extra  day in february.
 
+// => vikram bosak (Dropout Coder) , Go and search on youtube
 
-var year = 2020;
+var year = 2024;
 
-// @ts-ignore
-if (year % 4 === 0); {
+if (year % 4 === 0) {
+    if (year % 100 === 0) {
+        if (year % 400 === 0) {
+            console.log('The year ' + year + ' is a leap year');
+        } else {
+            console.log('The year ' + year + 'is not  a leap year');
+        }
 
-    // @ts-ignore
-    // @ts-ignore
-    // @ts-ignore
-    if (year % === 0); {
-
-        // @ts-ignore
-    }else {
-        console.log("The year " + year + "is not a leap year");
+    } else {
+        console.log('The year ' + year + ' is  a leap year');
     }
-    // @ts-ignore
-    else {
-        console.log("The year " + year + "is not a leap year");
-    }
-}
-console.log();
 
-
-const name = () => {
-
+} else {
+    console.log('The year ' + year + ' is not a leap year');
 
 }
