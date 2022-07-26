471d7c49f06307ec1175af8f3ce3c8b065392f6c
vikram-bosak <vikramkumarbosak69@gmail.com>
2022-07-26 14:21:44 +0530

    today is  my 2nd day of my masai journey

---
+++ masai-note.js
@@ -0,0 +1,16 @@
+
+// ->Date - 26/07/2022 , Time- 10:00 am To 11:30 am
+
+// / introduction to computer fundamentals , problem solving & critical thinking
+
+// => lecture notes class video recordings reference sites
+
+// => i am having doubts whom should i ask?
+
+// => communication ask questions  Not answers
+
+// => can i get placed in xyz company?
+
+// => Tutorial hell , click baits , easy paths , Google , stack overflow , blogs don't use shortcut and don't go to short ways
+
+=> course structure t
