a7e6634d0fd1730da28d43876777e4b59eef989c
vikram-bosak <vikramkumarbosak69@gmail.com>
2022-07-27 13:39:12 +0530

    today is my 3rd day of masai school

--- masai-note.js
+++ masai-note.js
@@ -13,4 +13,30 @@
 
 // => Tutorial hell , click baits , easy paths , Google , stack overflow , blogs don't use shortcut and don't go to short ways
 
-=> course structure t
+// => course structure t
+
+
+
+// ->Date - 26/07/2022 , Time- 02:30 am To 03:30 am
+
+// / introduction to cc101/EFM
+
+// =>corporate Expectations
+
+
+// ->Date - 26/07/2022 , Time- 06:00 am To 07:00 am
+
+// / introduction of DSA
+
+// =>save the and costs and invokes problem and technical hiring and core of computer science
+
+
+// => its require focus and willingness in dsa solving problem is fun
+
+
+
+// ->Date - 27/07/2022 , Time- 09:00 am To 09:30 am
+
+// =>scrum
+
+commnication skill is very important 
